a912c57345c9126b1c79f391656170987844dece
hallizh <hallizh1@gmail.com>
2016-08-05 23:19:02 +0000

    Button listener added and minimal styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.info('Starting up...')
+var codeFrom = document.getElementById("codeFrom");
+var codeTo = document.getElementById("codeTo");
+
+document.getElementById('generate').addEventListener('click', function() {
+	console.info(codeFrom.value)
+	console.info(codeTo.value)
+});
