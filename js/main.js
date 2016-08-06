a0dd16c48a7ab297965ac0aebcd67c6388bcb815
hallizh <hallizh1@gmail.com>
2016-08-06 03:54:10 +0000

    type mapping added to typescript

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var codeTo = document.getElementById("codeTo");
 codeFrom.value = `public class Movie 
 {
     public int ID { get; set; }
-    public string Title { get; set; }
+    public List<string> Title { get; set; }
     public DateTime ReleaseDate { get; set; }
     public string Genre { get; set; }
     public decimal Price { get; set; }
@@ -15,7 +15,7 @@ document.getElementById('generate').addEventListener('click', function() {
 	var codeArray = codeFrom.value.split('\n');
 	var thing = csharp.from(codeArray);
 		console.warn("Done!", thing);
-	
+
 	var newClass = typescript.to(thing);
 		console.warn("Done!", newClass);
 
--- js/typescript.js
+++ js/typescript.js
@@ -1,5 +1,45 @@
+function getType(type) {
+	var isGeneric = false;
+	var typeMap = {};
+	var insideBrackets = "";
+	typeMap['int'] = "number";
+	typeMap['decimal'] = "number";
+	typeMap['float'] = "number";
+	typeMap['double'] = "number";
+	typeMap['bigint'] = "number";
+	typeMap['long'] = "number";
+	typeMap['short'] = "number";
+	typeMap['uint'] = "number";
+	typeMap['ushort'] = "number";
+	typeMap['bool'] = "boolean";
+	typeMap['char'] = "string";
+	typeMap['string'] = "string";
+	typeMap['String'] = "string";
+	typeMap['DateTime'] = "string";
+	typeMap['object'] = "any";
+
+	if(type.indexOf("<") != -1) {
+		var regExp = /<(.*?)>/;
+		insideBrackets = regExp.exec(type);
+
+		isGeneric = true;
+	}
+
+	var returnVal = typeMap[type];
+	if(!returnVal)
+	{
+		return isGeneric ? insideBrackets[1] + "[]" : type;
+	}
+	else
+	{
+		return isGeneric ? insideBrackets[1] + "[]" : returnVal;
+	}
+}
+
 
 class typescript {
+
+
 	static from(codeArray) {
 
 	}
@@ -11,7 +51,7 @@ class typescript {
 			var data = classes[c];
 			newClass += "export class " + data.name + " {\n";
 			for(var i = 0; i < data.properties.length; i++) {
-				newClass += "\t" + data.properties[i].access + " " + data.properties[i].name + ": " + data.properties[i].type + ";\n";
+				newClass += "\t" + data.properties[i].access + " " + data.properties[i].name + ": " + getType(data.properties[i].type) + ";\n";
 			}
 			newClass += "} \n";
 
